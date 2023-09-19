04c724bf543b24e3ffef48136943f9f91ec69ace
Luis <alu.magallanes@gmail.com>
2023-09-19 13:15:57 -0300

    add fecha afiliacion

--- src/controllers/afiliadosObjet.js
+++ src/controllers/afiliadosObjet.js
@@ -1,17 +1,23 @@
 const datosAfiliados = {
-    tipo_documento: "",
-    DNI: "",
-    genero: "",
-    apellidos: "",
-    nombres: "",
-    cuil: "",
-    fecha_nacimiento: "",
-    localidad: "",
-    nacionalidad: "",
-    telefono: "",
-    mail: "",
-    estado_civil: "",
-    fecha_afiliacion: "",
-  };
-  
-  export default datosAfiliados;
+  tipo_documento: "",
+  DNI: "",
+  genero: "",
+  apellidos: "",
+  nombres: "",
+  cuil: "",
+  fecha_nacimiento: "",
+  localidad: "",
+  nacionalidad: "",
+  telefono: "",
+  mail: "",
+  estado_civil: "",
+  fechaAfiliacion: "",
+  empleador: "",
+  fechaAlta: "",
+  servicio: "",
+  funcion: "",
+  tipoInstitucion: "",
+
+};
+
+export default datosAfiliados;
