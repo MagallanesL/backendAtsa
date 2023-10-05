c21b8d762001a1fc81afebb9eb52a607274524ca
Luis <alu.magallanes@gmail.com>
2023-10-05 08:40:11 -0300

    quit empleador on route

--- src/routes/afiliados.js
+++ src/routes/afiliados.js
@@ -6,7 +6,6 @@ const router = Router();
 router.get("/", afiliadosControllers.getAfiliados);
 router.get("/:datos", afiliadosControllers.getDatos);
 router.post("/create", afiliadosControllers.addAfiliado);
-router.post("/empleador", afiliadosControllers.addEmpleadores);
 router.post("/api/familiar", afiliadosControllers.addFamiliar);
 router.delete("/api/afiliados/:dni", afiliadosControllers.deleteAfiliados);
 router.put("/api/afiliados/:dni", afiliadosControllers.updateAfiliados);
