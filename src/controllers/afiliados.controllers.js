c5c278231c889e9ab9b2bd65554c4d8a8c4cea8b
Luis <alu.magallanes@gmail.com>
2023-10-06 12:41:59 -0300

    add familiar sin funcionar aun

--- src/controllers/afiliados.controllers.js
+++ src/controllers/afiliados.controllers.js
@@ -1,6 +1,7 @@
 import mysql from "mysql2/promise";
 import config from "./../config.js";
 import  datosAfiliados  from "./afiliadosObjet.js";
+import familiarData from "./familiarAfiliado.js";
 
 const pool = mysql.createPool({
   host: config.host,
